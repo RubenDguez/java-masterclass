5c9acd1c146a3f0f750e112c8383f27f6cb53f63
Ruben Dominguez <argenis.dominguez@hotmail.com>
2025-02-19 18:14:50 -0500

    create: 07 Math Class Intro

---
+++ 07 Math Class Intro/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+        System.out.println(Math.pow(2, 3));
+        System.out.println(Math.sqrt(25));
+        System.out.println(Math.abs(-11));
+        System.out.println(Math.round(3.14));
+        System.out.println(Math.ceil(3.14));
+        System.out.println(Math.floor(3.14));
+        System.out.println(Math.max(5, 8));
+        System.out.println(Math.min(3, 1));
+    }
+}
